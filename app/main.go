dbe68d56ef14bf7488941eceaa9437d46f7326d5
neomopp <william.sawyer@infinityworks.com>
2021-02-06 20:37:19 +0000

    Translating CSV into map

---
+++ app/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "encoding/csv"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+)
+
+func readCsv(filePath string) [][]string {
+    f, err := os.Open(filePath)
+    if err != nil {
+        log.Fatal("Unable to read input file " + filePath, err)
+    }
+    defer f.Close()
+
+    csvReader := csv.NewReader(f)
+    records, err := csvReader.ReadAll()
+    if err != nil {
+        log.Fatal("Unable to parse file as CSV for " + filePath, err)
+    }
+
+    return records
+}
+
+func csvRecordsToMap(records [][]string) map[string]string{
+    m := make(map[string]string)
+    for i:=0; i<len(records); i+=1 {
+        m[records[i][0]] = records[i][1]
+    }
+    return m
+}
+
+func main() {
+    args := os.Args[1:]
+    input := strings.Join(args, " ")
+
+    recs := readCsv("matrix/l33t.csv")
+
+    translator := csvRecordsToMap(recs)
+    result := ""
+    for _, c := range input {
+        result = result+translator[string(c)]
+    }
+
+    fmt.Println(result)
+}
